e98217b8d4d4be3c1f0b0fe9b322b5efb45df34d
Kevin Terrier <kevin.terrier@le1817.com>
2022-11-22 15:54:15 +0100

    made login and register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Views/Home/Home'
+import Login from './Views/Login/Login'
+import Register from './Views/Register/Register'
+import { Navigate } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/connexion" element={<Login />} />
+        <Route path="/inscription" element={<Register />} />
       </Routes>
     </div>
   )
