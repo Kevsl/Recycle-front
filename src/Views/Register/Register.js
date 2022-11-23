07766104921de88f42bb96e4b59ae9adcbbf844c
Kevin Terrier <kevin.terrier@le1817.com>
2022-11-23 11:10:30 +0100

    fixed register

--- src/Views/Register/Register.js
+++ src/Views/Register/Register.js
@@ -14,6 +14,7 @@ const Register = () => {
   const [emailOk, setEmailOk] = useState(false)
   const [passOK, setPassOk] = useState(false)
   const [confirmpassOK, setConfirmPassOk] = useState(false)
+  const [similarPasswords, setSimilarPasswords] = useState(false)
 
   function handleEmail(value) {
     checkEmail(value) === true ? setEmailOk(true) : setEmailOk(false)
@@ -27,11 +28,11 @@ const Register = () => {
       : setConfirmPassOk(false)
   }
 
-  function handleSimilarPasswords(pass, confirm) {
-    if (pass === confirm) return true
+  function handleSimilarPasswords(password, value) {
+    password === value ? setSimilarPasswords(true) : setSimilarPasswords(false)
   }
   function handleRegister() {
-    if (passOK && emailOk && confirmPassword) {
+    if (passOK && emailOk && similarPasswords && confirmpassOK) {
       localStorage.setItem('isConnected', 'true')
       navigate('/')
     } else {
@@ -54,51 +55,47 @@ const Register = () => {
               Recycle
             </h2>
           </div>
-          <form className="mt-8 " action="#" method="POST">
-            <input type="hidden" name="remember" value="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                {email.length > 0 && emailOk === false && (
-                  <p className="text-xs text-red-reCycle">
-                    Le format de l’email est incorrect
-                  </p>
-                )}
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Adresse email"
-                  value={email}
-                  onChange={(e) => {
-                    handleEmail(e.target.value)
-                    setEmail(e.target.value)
-                  }}
-                />
-              </div>
-              <div>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none border-b-0  border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Mot de passe"
-                  value={password}
-                  onChange={(e) => {
-                    handlePassword(e.target.value)
-                    setPassword(e.target.value)
-                  }}
-                />
-              </div>
+          <input type="hidden" name="remember" value="true" />
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              {email.length > 0 && emailOk === false && (
+                <p className="text-xs text-red-reCycle">
+                  Le format de l’email est incorrect
+                </p>
+              )}
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Adresse email"
+                value={email}
+                onChange={(e) => {
+                  handleEmail(e.target.value)
+                  setEmail(e.target.value)
+                }}
+              />
+            </div>
+            <div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="relative block w-full appearance-none rounded-none border-b-0  border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Mot de passe"
+                value={password}
+                onChange={(e) => {
+                  handlePassword(e.target.value)
+                  setPassword(e.target.value)
+                }}
+              />
             </div>
             <input
-              id="password"
-              name="password"
+              id="confirm-password"
+              name="confirm-password"
               type="password"
-              autocomplete="current-password"
               required
               className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               placeholder="Confirmer votre mot de passe"
@@ -109,65 +106,68 @@ const Register = () => {
                 setConfirmPassword(e.target.value)
               }}
             />
-            {password.length > 0 && passOK === false && (
-              <p className="text-xs text-red-reCycle">
-                Le mot de passe doit avoir 1 majuscule,1minuscule,1 caractère
-                spécial, 8 caractères minimum
-              </p>
-            )}
-            {confirmPassword.length > 0 && confirmpassOK === false && (
-              <p className="text-xs text-red-reCycle">
-                La confirmation du mot de passe doit avoir 1
-                majuscule,1minuscule,1 caractère spécial, 8 caractères minimum
-              </p>
-            )}
-            <div className="w-full flex items-center justify-center flex-col my-4">
-              <button
-                className="bg-reCycle-green text-white rounded-xl px-6 w-48 mx-auto mt-2"
-                onClick={() => {
-                  handleRegister()
-                }}
-              >
-                S'inscrire
-              </button>
-              <p className="mt-2">où</p>
-              <button
-                className="text-reCycle-green bg-white rounded-xl px-6 w-48 mx-auto border border-reCycle-green mt-2"
-                onClick={() => {
-                  navigate('/connexion')
-                }}
-              >
-                Se connecter
-              </button>
-            </div>
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                />
-                <label
-                  for="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
+          </div>
 
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot your password?
-                </a>
-              </div>
+          {password.length > 0 && passOK === false && (
+            <p className="text-xs text-red-reCycle">
+              Le mot de passe doit avoir 1 majuscule,1minuscule,1 caractère
+              spécial, 8 caractères minimum
+            </p>
+          )}
+          {confirmPassword.length > 0 && confirmpassOK === false && (
+            <p className="text-xs text-red-reCycle">
+              La confirmation du mot de passe doit avoir 1
+              majuscule,1minuscule,1 caractère spécial, 8 caractères minimum
+            </p>
+          )}
+          {!similarPasswords && confirmPassword.length > 0 && (
+            <p className="text-xs text-red-reCycle">
+              Le mot de passe et sa confirmation doivent être identiques
+            </p>
+          )}
+          <div className="w-full flex items-center justify-center flex-col my-4">
+            <button
+              className="bg-reCycle-green text-white rounded-xl px-6 w-48 mx-auto mt-2"
+              onClick={() => {
+                handleRegister()
+              }}
+            >
+              S'inscrire
+            </button>
+            <p className="mt-2">où</p>
+            <button
+              className="text-reCycle-green bg-white rounded-xl px-6 w-48 mx-auto border border-reCycle-green mt-2"
+              onClick={() => {
+                navigate('/connexion')
+              }}
+            >
+              Se connecter
+            </button>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label
+                for="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
             </div>
 
-            <div></div>
-          </form>
+            <div className="text-sm">
+              <p className="font-medium text-indigo-600 hover:text-indigo-500">
+                Forgot your password?
+              </p>
+            </div>
+          </div>
+
+          <div></div>
         </div>
       </div>
     </div>
