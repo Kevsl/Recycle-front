f9089b4c148cea84484ae5ef8bc5f831af41de00
Kevin Terrier <kevin.terrier@le1817.com>
2023-06-09 00:29:38 +0200

    feat: made file upload

--- src/Components/UploadWidget.js
+++ src/Components/UploadWidget.js
@@ -1,22 +1,33 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const UploadWidget = ({ setAcceptedListingImages, acceptedListingImages }) => {
   const cloudinaryRef = useRef()
   const widgetRef = useRef()
+  const [filesNames, setFilesNames] = useState([])
+
+  useEffect(() => {
+    console.log(acceptedListingImages)
+  }, [acceptedListingImages])
 
   useEffect(() => {
     cloudinaryRef.current = window.cloudinary
     widgetRef.current = cloudinaryRef.current.createUploadWidget(
       {
-        cloudName: 'dk1vkcts1',
-        uploadPreset: 'arbcc9rc',
+        cloudName: process.env.REACT_APP_CDN_NAME,
+        uploadPreset: process.env.REACT_APP_CDN_KEY,
         resourceType: 'image',
         allowedFormats: ['jpg', 'jpeg', 'png'],
         maxFiles: 3,
       },
       function (err, res) {
+        console.log(res)
         if (!err && res && res.event === 'success') {
           setAcceptedListingImages((prevFiles) => [
+            ...prevFiles,
+            res.info.secure_url,
+          ])
+
+          setFilesNames((prevFiles) => [
             ...prevFiles,
             res.info.original_filename,
           ])
@@ -39,13 +50,13 @@ const UploadWidget = ({ setAcceptedListingImages, acceptedListingImages }) => {
 
         <div className="flex items-center flex-col justify-center">
           <p className="fa-solid fa-plus text-center border border-gray-recycle w-8 h-8 px-2 py-2 rounded-full mx-auto my-2 text-gray-recycle"></p>
-          {acceptedListingImages.length > 0 && (
+          {filesNames.length > 0 && (
             <div>
               <p>Fichiers téléchargés</p>
 
               <ul>
-                {acceptedListingImages.map((file) => (
-                  <li key={file}>{file}</li>
+                {filesNames.map((file, i) => (
+                  <li key={`${file}${i}`}>{file}</li>
                 ))}
               </ul>
             </div>
--- src/Service/listingService.js
+++ src/Service/listingService.js
@@ -69,7 +69,6 @@ export async function getMyListings(id) {
     return res.data
   })
 }
-
 export async function createListing(
   title,
   description,
@@ -81,34 +80,41 @@ export async function createListing(
   city,
   latitude,
   longitude,
-  formData
+  files
 ) {
-  let url = `${'http://127.0.0.1/'}listing/new2`
+  let url = `${'http://127.0.0.1/'}listing/new`
   let id = localStorage.getItem('id')
-
   const config = {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-      'Content-Type': 'multipart/form-data',
-    },
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
   }
 
-  formData.append('city', city)
-  formData.append('country', 'France')
-  formData.append('description', description)
-  formData.append('title', title)
-  formData.append('fkSubCategory', listingSubCategoryId)
-  formData.append('fkListingStatus', 1)
-  formData.append('fkListingType', listingTypeId)
-  formData.append('fkUser', localStorage.getItem('id'))
-  formData.append('longitude', longitude)
-  formData.append('latitude', latitude)
-  formData.append('postCode', postCode)
+  return axios
+    .post(
+      url,
 
-  return axios.post(url, formData, config).then((res) => {
-    return res.status
-  })
+      {
+        city: city,
+        country: 'France',
+        description: description,
+        title: title,
+        fkSubCategory: listingSubCategoryId,
+        fkListingStatus: 1,
+        fkListingType: listingTypeId,
+        fkSubListingType: listingSubTypeId,
+        fkProfile: id,
+        latitude: latitude,
+        longitude: longitude,
+        postCode: postCode,
+        fkUser: localStorage.getItem('id'),
+        images: files,
+      },
+      config
+    )
+    .then((res) => {
+      return res.status
+    })
 }
+
 export async function getCitiesList(id) {
   let url = `https://api-adresse.data.gouv.fr/search/?q=${id}&type=municipality`
 
